171000c6b9fa6e9a1b1c1380484bf296f1740449
NelsonConde <nelsonenrriquect@ufps.edu.co>
2026-07-04 21:15:34 -0500

    Inicializado backend de sistema casa de cambios

---
+++ src/main/java/com/casacambios/backend/SistemaCasaDeCambiosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.casacambios.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCasaDeCambiosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCasaDeCambiosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casacambios/backend/SistemaCasaDeCambiosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casacambios.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCasaDeCambiosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
